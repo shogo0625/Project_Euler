23d48447f62f295a5295c52adbb36139528c3e5b
shogo0625 <shogo19940625@gmail.com>
2020-07-05 17:22:13 +0900

    Learning Grammar

---
+++ paiza.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+  // 文字列のN文字目を取得
+  console.log('hello'[0]); // => h
+  console.log('hello, world'.charAt(0)); // => h
+  // hello 文字列を置き換え
+  console.log('hello, world'.replace('hello', 'goodbye')); // => "goodbye, world"
+  // 大文字・小文字(toDownCase)
+  console.log('hello'.toUpperCase());
+}
