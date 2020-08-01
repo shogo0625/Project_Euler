a8b73a18307c5247e461b19a5ab4a936a9fdc1aa
shogo0625 <shogo19940625@gmail.com>
2020-08-01 14:44:56 +0900

    Learning JSPrimer

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -8,10 +8,43 @@ function fetchUserInfo(userId) {
       } else {
         return response.json().then(userInfo => {
           // JSONパースされたオブジェクトが渡される
-          console.log(userInfo);
+          // HTMLの組み立て　タグ関数（第1引数にテンプレートリテラルの配列、第2引数にその値が渡される）
+          const view = escapeHTML`
+            <h4>${userInfo.name} (@${userInfo.login})</h4>
+            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+            <dl>
+              <dt>Location</dt>
+              <dd>${userInfo.location}</dd>
+              <dt>Repositories</dt>
+              <dd>${userInfo.public_repos}</dd>
+            </dl>
+          `;
+          // HTMLの挿入
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     }).catch(error => {
       console.error(error);
     });
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+// タグ関数（第1引数にテンプレートリテラルの配列、第2引数にその値が渡される）
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 }
