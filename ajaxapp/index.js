b9114213395c9d30ef3da21bc7d7f5ab140cfcb5
shogo0625 <shogo19940625@gmail.com>
2020-08-01 19:36:45 +0900

    Ajax通信/データを表示する

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,32 +1,46 @@
+function main() {
+  fetchUserInfo("js-primer-example")
+    // ここではJSONオブジェクトで解決されるPromise
+    .then((userInfo) => createView(userInfo))
+    // ここではHTML文字列で解決されるPromise
+    .then((view) => displayView(view))
+    // Promiseチェーンでエラーがあった場合はキャッチされる
+    .catch((error) => {
+      console.error(`エラーが発生しました (${error})`);
+    });
+}
+
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  // fetchの戻り値のPromiseをreturnする
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
-      console.log(response.status);
       // エラーレスポンスが返されたことを検知する
       if (!response.ok) {
-        console.error("エラーレスポンス", response);
+        // エラーレスポンスからRejectedなPromiseを作成して返す
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
       } else {
-        return response.json().then(userInfo => {
-          // JSONパースされたオブジェクトが渡される
-          // HTMLの組み立て　タグ関数（第1引数にテンプレートリテラルの配列、第2引数にその値が渡される）
-          const view = escapeHTML`
-            <h4>${userInfo.name} (@${userInfo.login})</h4>
-            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-            <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-            </dl>
-          `;
-          // HTMLの挿入
-          const result = document.getElementById("result");
-          result.innerHTML = view;
-        });
+        // JSONオブジェクトで解決されるPromiseを返す
+        return response.json();
       }
-    }).catch(error => {
-      console.error(error);
     });
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+    <dt>Location</dt>
+    <dd>${userInfo.location}</dd>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+  </dl>
+`;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
