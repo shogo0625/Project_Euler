625bddae1e5d3a286ac55a6794d708ff007c14de
shogo0625 <shogo19940625@gmail.com>
2020-08-01 11:51:24 +0900

    Learning JSPrimer

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,8 +1,17 @@
-const heading = document.querySelector("h2");
-
-const headingText = heading.textContent;
-
-const button = document.createElement("button");
-button.textContent = "Push Me";
-
-document.body.appendChild(button);
+function fetchUserInfo(userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      console.log(response.status);
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+        console.error("エラーレスポンス", response);
+      } else {
+        return response.json().then(userInfo => {
+          // JSONパースされたオブジェクトが渡される
+          console.log(userInfo);
+        });
+      }
+    }).catch(error => {
+      console.error(error);
+    });
+}
--- jsprimer.js
+++ jsprimer.js
@@ -1133,3 +1133,50 @@
   console.log(Math.trunc(1.3)); // => 1
   console.log(Math.trunc(-1.3)); // => -1
 }
+
+// Fetch API　HTTP通信を行ってリソースを取得するためのAPI
+// fetch メソッドでリクエスト作成　プロミスを返す
+// encodeURIComponentは/や%などURLとして特殊な意味を持つ文字列をただの文字列として扱えるようにエスケープする関数
+{
+  function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+      .then(response => {
+        console.log(response.status);
+        // エラーレスポンスが返されたことを検知する
+        if (!response.ok) {
+          console.error("エラーレスポンス", response);
+        } else {
+          return response.json().then(userInfo => {
+            // JSONパースされたオブジェクトが渡される
+            console.log(userInfo);
+          });
+        }
+      }).catch(error => {
+        console.error(error);
+      });
+  }
+}
+// XMLHttpRequest（XHR）はFetch APIと同じくHTTP通信を行うためのAPI(現在はFetch APIの方が一般的)
+{
+  function fetchUserInfo(userId) {
+    // リクエストを作成する
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://api.github.com/users/${encodeURIComponent(userId)}`);
+    request.addEventListener("load", () => {
+      // リクエストが成功したかを判定する
+      // Fetch APIのresponse.okと同等の意味
+      if (request.status >= 200 && request.status < 300) {
+        // レスポンス文字列をJSONオブジェクトにパースする
+        const userInfo = JSON.parse(request.responseText);
+        console.log(userInfo);
+      } else {
+        console.error("エラーレスポンス", request.statusText);
+      }
+    });
+    request.addEventListener("error", () => {
+      console.error("ネットワークエラー");
+    });
+    // リクエストを送信する
+    request.send();
+  }
+}
